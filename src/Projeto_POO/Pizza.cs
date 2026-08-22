e8813d7b96bb47d50220943f3fbee435b8221bc4
Nathan Victor Morais Nogueira <nathan3345.victor@gmail.com>
2026-08-21 23:37:25 -0300

    treinando códigos

--- src/Projeto_POO/Pizza.cs
+++ src/Projeto_POO/Pizza.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 
 
-namespace XulambsFoods {    
-    public class Pizza {
+namespace XulambsFoods
+{
+    public class Pizza
+    {
 
         /// <summary>
         /// Lembre-se:
@@ -20,7 +22,8 @@ namespace XulambsFoods {
         #endregion
 
         #region construtores
-        public Pizza() {
+        public Pizza()
+        {
             _descricao = "Pizza";
             _maxIngredientes = 8;
             _precoBase = 29d;
@@ -28,40 +31,52 @@ namespace XulambsFoods {
             _valorPorAdicional = 5d;
         }
 
-        public Pizza(int adicionais) {
-        
+        public Pizza(int adicionais)
+        {
+
         }
         #endregion
 
         #region métodos privados
-        private double ValorAdicionais() {
-                
+        private double ValorAdicionais()
+        {
+
+            return _quantIngredientes * _valorPorAdicional;
+
         }
 
-        private void ModificarDescricao() {
+        private void ModificarDescricao()
+        {
             _descricao = $"Pizza com {_quantIngredientes} adicionais";
         }
 
-        private bool PodeAdicionar(int quantos) {
-                
+        private bool PodeAdicionar(int quantos)
+        {
+            return (quantos >= 0 && quantos + _quantIngredientes <= _maxIngredientes);
+          
+
         }
         #endregion
 
         #region métodos públicos
-        public double CalcularValorFinal() {
+        public double CalcularValorFinal()
+        {
             return _precoBase + ValorAdicionais();
         }
 
-        public int AdicionarIngredientes(int quantos) {
-            if (PodeAdicionar(quantos)) {
+        public int AdicionarIngredientes(int quantos)
+        {
+            if (PodeAdicionar(quantos))
+            {
                 _quantIngredientes = _quantIngredientes + quantos;
                 ModificarDescricao();
             }
             return _quantIngredientes;
         }
 
-        public string GerarCupom() {
-                
+        public string GerarCupom()
+        {
+
         }
         #endregion
 
